6e11537cd80498bb8a1bd01af73717edb13d98e0
Pedro Arvela <jp.arvela@gmail.com>
2016-05-26 19:35:40 +0100

    Initial implementation of reactive villager

--- AIScripts/Deciders/DeciderVillagerReactive.cs
+++ AIScripts/Deciders/DeciderVillagerReactive.cs
@@ -6,26 +6,33 @@ using System;
 
 namespace Scripts.AI.Deciders
 {
-    public class DeciderVillagerReactive : Decider
-    {
+	public class DeciderVillagerReactive : Decider
+	{
+		public override Actions.Action Decide (Perception[] perceptions)
+		{
+			// If there are Orcs, deal with them
+			// Otherwise, deal with resources
+			// If none of the above apply, wander around.
 
+			if (Array.Exists (perceptions, p => p.Name == "SeeOrcs")) {
+				SeeOrc[] orcs = (SeeOrc[]) Array.FindAll (perceptions, p => p.Name == "SeeOrcs");
+				GameObject closestOrc = orcs [0].perceptionTarget;
 
+				// Find the closest orc
+				foreach(SeeOrc orc in orcs) {
+					float oldDistance = Vector3.Distance (this.transform.position, closestOrc.transform.position);
+					float newDistance = Vector3.Distance (this.transform.position, orc.perceptionTarget.transform.position);
+					if (newDistance <= oldDistance) {
+						closestOrc = orc.perceptionTarget;
+					}
+				}
 
-        public override Actions.Action Decide(Perception[] perceptions)
-        {
-            throw new NotImplementedException();
-        }
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-    }
+				return new AttackTarget (closestOrc);
+			} else if (Array.Exists (perceptions, p => p.Name == "SeeResources")) {
+				//TODO: Deal with resources
+			} else {
+				return new Wander ();
+			}
+		}
+	}
 }
